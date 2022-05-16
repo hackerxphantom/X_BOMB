b3bd3a665c164ad5188aac52faaa2c41bb40d375
PH4N70M (X PHANTOM) <70594016+XPH4N70M@users.noreply.github.com>
2022-05-16 10:55:02 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -27,21 +27,21 @@ banner() {
     clear
     echo -e "\e[1;31m"
     if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing TBomb'
+        echo 'Introducing X_BOMB'
     else
-        figlet TBomb
+        figlet X_BOMB
     fi
     if ! [ -x "$(command -v toilet)" ]; then
         echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
     else
         echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        toilet -f mono12 -F border X_PH4N70M (X PHANTOM)
     fi
     echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;32m           Telegram: https://t.me/X_PH4N7OM \e[0m"
+    echo -e "\e[4;32m   JOIN US: https://bit.ly/3wiPUko \e[0m"
     echo " "
-    echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
+    echo "NOTE: Kindly move to the PIP version Of X_BOMB for more stability."
     echo " "
 }
 
@@ -98,7 +98,7 @@ else
     echo .
     echo .
     install_deps
-    echo This Script Was Made By SpeedX > .update
+    echo This Script Was Made By X PHANTOM (X_PH4N7OM)> .update
     echo 'Requirements Installed....'
     pause
 fi
@@ -127,7 +127,7 @@ do
         echo -e "\e[1;34m Downloading Latest Files..."
         rm -f .update
         $PYTHON bomber.py --update
-        echo -e "\e[1;34m RUN TBomb Again..."
+        echo -e "\e[1;34m RUN X_BOMB Again..."
         pause
         exit
     elif [ $ch -eq 5 ];then
